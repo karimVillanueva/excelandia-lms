a0d34c27fefc398aa3cdf32b43953a15fc307c20
karimVillanueva <45979912+karimVillanueva@users.noreply.github.com>
2026-06-22 22:39:52 -0600

    add directus token

--- lib/directus.ts
+++ lib/directus.ts
@@ -1,5 +1,13 @@
-import { createDirectus, rest } from "@directus/sdk";
+import { createDirectus, rest, staticToken } from "@directus/sdk";
 
-export const directus = createDirectus(
-    process.env.DIRECTUS_URL!
-).with(rest());
+if (!process.env.DIRECTUS_URL) {
+    throw new Error("DIRECTUS_URL is required");
+}
+
+if (!process.env.DIRECTUS_TOKEN) {
+    throw new Error("DIRECTUS_TOKEN is required");
+}
+
+export const directus = createDirectus(process.env.DIRECTUS_URL)
+    .with(staticToken(process.env.DIRECTUS_TOKEN))
+    .with(rest());
